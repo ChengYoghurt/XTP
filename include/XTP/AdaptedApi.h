b853d67ee6e52c52f88ae3e615cad259466f6026
yimdx <1595732817@qq.com>
2022-04-07 20:11:46 +0800

    declarion added

--- include/XTP/AdaptedApi.h
+++ include/XTP/AdaptedApi.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "AdaptedTypes.h"
+#include "XTP/AdaptedTypes.h"
 #include "TraderTypes.h"
 #include "WCApi.h"
 #include "spdlog/spdlog.h"
@@ -25,6 +25,9 @@ protected:
     virtual void OnDisconnected(uint64_t session_id, int reason) ; 
     virtual void OnQueryPosition(ApiPosition *position, ApiText *error_info, ApiRequestID request_id, bool is_last, uint64_t session_id) ;
     virtual void OnQueryAsset(ApiBalance *asset, ApiText *error_info, ApiRequestID request_id, bool is_last, uint64_t session_id) ;
+    virtual void OnOrderEvent(ApiOrderReport *order_info, ApiText error_info, uint64_t session_id);
+    virtual void OnTradeEvent(ApiTradeReport *trade_info, uint64_t session_id);
+    virtual void OnCancelOrderError(ApiOrderCancelReject *cancel_info, ApiText *error_info, uint64_t session_id);
 
 protected:
     uint32_t trade_id_;
@@ -38,19 +41,26 @@ public:
     AdaptedApi() ;
     virtual ~AdaptedApi() ;
     virtual std::string version() const noexcept ;
-    
-    
-    virtual error_id_t  login(WCLoginRequest const& request) ;
+    virtual error_id_t login(WCLoginRequest const& request) ;
+    int get_trading_day() ;
+    ApiRequestID get_request_id() ;
+    error_id_t register_spi(std::unique_ptr<WCSpi> p_spi) ;
+    error_id_t place_order(WCOrderRequest const& request) ;
+    error_id_t cancel_order(WCOrderCancelRequest const& request) ;
+    error_id_t query_balance() ;
+    error_id_t query_position(WCPositionQueryRequest const& request) ;
+    error_id_t query_credit_balance() ;
+    error_id_t place_basket_order(WCBasketOrderRequest const& request) ;
+    error_id_t cancel_basket_order(WCBasketOrderCancelRequest const& request) ;
     
 protected:
     ApiRequestID get_request_id();
     static order_status_t simplify_status(ApiOrderStatus) ;
 protected:
-    BrokerApi * p_trader_api_;
+    BrokerApi * p_broker_api_;
     uint32_t trade_id_;
     size_t request_id_ = 0;
     uint64_t session_id_;
-    ApiTradeUnitID trade_unit_;
     std::unique_ptr<AdaptedSpi> p_spi_;
     std::shared_ptr<spdlog::logger> p_logger_;
 
